780ca3a51c26338e0d66a87e23e3f9b2c7a984b1
Daniel Chicchon <danielchicchon@gmail.com>
2024-03-25 23:20:09 -0700

    update logs, module.json and gitignore file

--- scripts/mobile.js
+++ scripts/mobile.js
@@ -1,9 +1,11 @@
-console.log("Hello World! This code runs immediately when the file is loaded.");
+console.log("FOUNDRY_MOBILE");
 
 Hooks.on("init", function () {
-    console.log("This code runs once the Foundry VTT software begins its initialization workflow.");
+    console.log("FOUNDRY_MOBILE INIT");
 });
 
 Hooks.on("ready", function () {
-    console.log("This code runs once core initialization is ready and game data is available.");
+    console.log("FOUNDRY_MOBILE READY");
+
+    // on ready we should 
 });
